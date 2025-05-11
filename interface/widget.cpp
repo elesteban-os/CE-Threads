2e548ceeb7ebaed3496701e1956ffa7baf6ee6c8
AlbertVega <albertbabol2504@estudiantec.cr>
2025-05-10 21:28:36 -0600

    animacion de auto

--- interface/widget.cpp
+++ interface/widget.cpp
@@ -1,6 +1,9 @@
 #include "widget.h"
 #include "ui_widget.h"
 #include <iostream>
+#include <QPixmap>
+#include <QDebug>
+#include <QPropertyAnimation>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
@@ -17,5 +20,13 @@ Widget::~Widget()
 
 void Widget::on_pushButton_clicked()
 {
-    std::cout << "test";
+    QPropertyAnimation *anim = new QPropertyAnimation(ui->imagenDeportivo, "geometry");
+    anim->setDuration(10000); // duración en milisegundos (2 segundos)
+
+    QRect startRect = ui->imagenDeportivo->geometry();
+    QRect endRect = startRect.translated(600, 0); // mover 200 píxeles a la derecha
+
+    anim->setStartValue(startRect);
+    anim->setEndValue(endRect);
+    anim->start(QAbstractAnimation::DeleteWhenStopped);
 }
